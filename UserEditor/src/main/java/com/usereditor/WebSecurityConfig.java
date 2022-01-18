396ef81b8f1dbdd91b711eba06d746ba1b117c15
Lao <lcepeda99556@universidadean.edu.co>
2022-01-17 20:47:41 -0500

    Added logout and logged in index

--- UserEditor/src/main/java/com/usereditor/WebSecurityConfig.java
+++ UserEditor/src/main/java/com/usereditor/WebSecurityConfig.java
@@ -48,12 +48,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-			.antMatchers("/billing_info").authenticated()
+			.antMatchers("/indexlogged").authenticated()
 			.anyRequest().permitAll()
 			.and()
 			.formLogin()
 				.usernameParameter("email")
-				.defaultSuccessUrl("/billing_info")
+				.defaultSuccessUrl("/indexlogged")
 				.permitAll()
 			.and()
 			.logout().logoutSuccessUrl("/").permitAll();
